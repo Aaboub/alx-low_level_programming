a11ef02c437f2eab81974d90b404c5eceb5bc203
Anouar A <a.aaboub@gmail.com>
2023-07-13 06:29:49 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include "unistd.h"
+#include "string.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: printf a string to the console
+ *
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, strlen(msg));
+	return (1);
+}
