531ad895cb85b568a5d85142da94cae1b96a9fb9
Anouar A <a.aaboub@gmail.com>
2023-07-21 17:23:09 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - is lower
+ *
+ * Description: desc
+ * @c: input
+ *
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (islower(c));
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+    _putchar('a');	
+    r = islower('o');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
