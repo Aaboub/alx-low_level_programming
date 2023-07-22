b52d595b9a74cc4a4af2a186592e35143f06e364
Anouar A <a.aaboub@gmail.com>
2023-07-22 03:40:23 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print
+ *
+ * Description: desc
+ * @n: n
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+	}
+	printf("%d\n", n);
+}
