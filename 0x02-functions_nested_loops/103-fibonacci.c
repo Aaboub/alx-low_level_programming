c60c1adf20029247b03b6c4b97810ad28d5fe075
Anouar A <a.aaboub@gmail.com>
2023-07-22 04:55:53 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - test
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long fib1, fib2, tmp;
+	long sum = 2;
+
+	fib1 = 1;
+	fib2 = 2;
+	tmp = fib1 + fib2;
+	while (tmp < 4000000){
+		if (tmp % 2)
+			sum += tmp;
+		fib1 = fib2;
+		fib2 = tmp;
+		tmp = fib1 + fib2;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
