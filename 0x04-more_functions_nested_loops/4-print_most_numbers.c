44405c63f0e8dd3c3a45376e7757bf59db91d8ed
Anouar A <a.aaboub@gmail.com>
2023-07-22 17:32:25 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  print_most_numbers- Entry
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	short i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 | i == 4)
+			continue;
+		_putchar('0' + i);
+	}
+		_putchar('\n');
+}
