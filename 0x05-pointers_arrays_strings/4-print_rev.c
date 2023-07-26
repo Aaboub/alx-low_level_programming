bd8683c3381a2d3df7a7a4cd02a94eb877b3fb93
Anouar A <a.aaboub@gmail.com>
2023-07-26 14:51:30 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+/**
+ * print_rev - Entry
+ *
+ * @s: param
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = (int)strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,9 @@
-void _puts(char *str);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void _putchar(char c);
+void print_rev(char *s);
+
+#endif /* MAIN_H */
