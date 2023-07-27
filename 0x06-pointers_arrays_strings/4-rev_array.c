77fa0b3cd2db8b4e9db4ab3149204103e6e75c3d
Anouar A <a.aaboub@gmail.com>
2023-07-27 13:42:59 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <string.h>
+
+/**
+ * reverse_array - Entry point
+ *
+ * @a: param
+ * @n: param
+ *
+ * Return: pointer to string
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
