2e37fef323087d46d44fb4d35a4bb5c55aff9cec
Anouar A <a.aaboub@gmail.com>
2023-07-27 13:33:49 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcpy - Entry point
+ *
+ * @dest: param
+ * @src: param
+ *
+ * Return: pointer to string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
