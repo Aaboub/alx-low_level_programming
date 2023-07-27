00a64520fb3a56dd5c9e87037f77267725e29a4a
Anouar A <a.aaboub@gmail.com>
2023-07-27 10:28:32 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcat - Entry point
+ *
+ * @dest: param
+ * @src: param
+ *
+ * Return: pointer to string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
