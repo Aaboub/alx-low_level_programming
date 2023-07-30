531d21aa64beb503643e7acf7d7de2ec6eeeace9
Anouar A <a.aaboub@gmail.com>
2023-07-30 12:23:25 -0700

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *
+ * @n: param
+ *
+ * Return: pointer to string
+ */
+void print_number(int n)
+{
+	int i = 0;
+	int j;
+	int k;
+	int tmp;
+	int l;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	tmp = n;
+
+	while (n > 0)
+	{
+		i++;
+		n = n / 10;
+	}
+
+	k = 1;
+	for (j = 1; j < i; j++)
+	{
+		k = k * 10;
+	}
+
+	while (tmp > 0)
+	{
+		l = tmp / k;
+		_putchar('0' + l);
+		tmp = tmp - (l * k);
+		k = k / 10;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,1 +1,2 @@
-char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
