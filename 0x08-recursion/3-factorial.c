a54a4d090c3a44eec7db4be3231da5ea9bb42f55
Anouar A <a.aaboub@gmail.com>
2023-08-05 11:27:39 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * factorial - Entry point
+ *
+ * @n: param
+ *
+ * Return: int
+ */
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (factorial(n - 1));
+}
