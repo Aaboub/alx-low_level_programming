4c6726749746a2d13955995b8cb4047d7148c8ef
Anouar A <a.aaboub@gmail.com>
2023-08-08 07:30:40 -0700

    fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,12 @@
 char *_strdup(char *str)
 {
 	char *p;
-	unsigned int len = (unsigned int)strlen(str);
+	unsigned int len;
 
 	if (!str)
 		return (NULL);
+
+	len = (unsigned int)strlen(str);
 	p = malloc((len + 1) * sizeof(char));
 	if (p)
 		memcpy(p, str, len + 1);
