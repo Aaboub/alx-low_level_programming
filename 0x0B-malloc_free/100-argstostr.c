fd507bb5f5d14684c61d47c86d7bfa3ffbc9d5e6
Anouar A <a.aaboub@gmail.com>
2023-08-08 14:13:11 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - Entry
+ *
+ * @ac: param
+ * @av: param
+ *
+ * Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p, *q;
+	int i, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += (unsigned int)strlen(av[i]);
+	}
+
+	p = malloc(len + 8 + 1);
+	if (p)
+	{
+		q = p;
+		for (i = 0; i < ac; i++)
+		{
+			len = (int)strlen(av[i]);
+			memcpy(q, av[i], len);
+			memcpy(q + len, "\n", 1);
+			q = q + len + 1;
+		}
+		return (p);
+	}
+	else
+		return (NULL);
+}
