7980a6583f8679a5eea9092a567e16f886d153b5
Anouar A <a.aaboub@gmail.com>
2023-08-09 04:16:07 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * @arc: param
+ * @arv: param
+ *
+ * Return: int
+ */
+int main(int arc, char **arv)
+{
+	(void)arv;
+	printf("%d\n", arc - 1);
+	return (0);
+}
