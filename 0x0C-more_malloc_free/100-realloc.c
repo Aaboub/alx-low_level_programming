81d300935a434dd6f950c0092c2c91a913a3ef70
Anouar A <a.aaboub@gmail.com>
2023-08-12 13:58:23 -0700

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - Entry
+ *
+ * @ptr: param
+ * @old_size: param
+ * @new_size:param
+ *
+ * Return: pointer
+ */
+int *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	new_ptr = malloc(new_size);
+	if (new_ptr)
+	{
+		memcpy(new_ptr, ptr, new_size);
+		free(ptr);
+		return (new_ptr);
+	}
+	return (ptr);
+
+}
