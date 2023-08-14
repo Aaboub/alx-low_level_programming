91d83a0c1783774df4bd10286317840fed23408b
Anouar A <a.aaboub@gmail.com>
2023-08-14 14:27:38 -0700

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
+
+/**
+ * _strcpy - Entry
+ *
+ * @dest: param
+ * @src: pram
+ *
+ * Return: void
+ */
+void _strcpy(char *dest, char *src)
+{
+	while
+		(*dest++ == *src++);
+}
+
 
 /**
  * new_dog - Entry point
@@ -19,8 +35,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!dog)
 		return (NULL);
 
-	dog->name = name;
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (!dog->name)
+	{
+		free(dog);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (!dog->owner)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	_strcpy(dog->owner, owner);
 	dog->age = age;
-	dog->owner = owner;
 	return (dog);
 }
