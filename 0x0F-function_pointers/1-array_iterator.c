403deee3c34ece7aa20bdee74623793a28d4b505
Anouar A <a.aaboub@gmail.com>
2023-08-16 13:30:39 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - prints a name
+ * @array: name of the person
+ * @size: pointer to function
+ * @action: pointer to func
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		action(array[i]);
+	}
+}
