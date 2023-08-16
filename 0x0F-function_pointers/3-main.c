baecf73ee370e0f4bf92af1c806d75941f1a12b8
Anouar A <a.aaboub@gmail.com>
2023-08-16 15:06:15 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: param
+ * @argv: param
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int a, b;
+	char *op;
+
+	int (*p(char *s))(int, int);
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = argv[2];
+
+	if (strcmp(op, "+") && strcmp(op, "-") && strcmp(op, "*") &&
+			strcmp(op, "/") && strcmp(op, "%"))
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((!strcmp(op, "/") || strcmp(op, "%")) && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - Entry
+ * @a: param
+ * @b: param
+ *
+ * Return: a+b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Entry
+ * @a: param
+ * @b: param
+ *
+ * Return: a+b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Entry
+ * @a: param
+ * @b: param
+ *
+ * Return: a+b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Entry
+ * @a: param
+ * @b: param
+ *
+ * Return: a+b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Entry
+ * @a: param
+ * @b: param
+ *
+ * Return: a+b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
