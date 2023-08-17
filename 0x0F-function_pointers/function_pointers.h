c9e65bd2ea0b748074f11f7633b040191cae988b
Anouar A <a.aaboub@gmail.com>
2023-08-17 12:06:34 -0700

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,6 @@
-void array_iterator(int *array, size_t size, void (*action)(int));
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
