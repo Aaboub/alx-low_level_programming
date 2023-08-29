657a049127868ba726bb36ac6eb1e19fe5ad3c49
Anouar A <a.aaboub@gmail.com>
2023-08-29 14:44:11 -0700

    2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - entry
+ * @head: param
+ * @n: param
+ *
+ * Return: ptr
+ */
+list_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
